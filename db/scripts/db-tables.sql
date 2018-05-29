a073d9a658f55c8c4b3d86107729a2bbec832c90
Terrance Corley <terrancecorley@outlook.com>
2018-05-29 11:45:39 -0700

    created test db

---
+++ db/scripts/db-tables.sql
@@ -0,0 +1,44 @@
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS snips CASCADE;
+DROP TABLE IF EXISTS tags CASCADE;
+DROP TABLE IF EXISTS snips_tags;
+
+CREATE TABLE users(
+  id serial PRIMARY KEY,
+  username text NOT NULL UNIQUE,
+  password text NOT NULL,
+  created timestamp DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE snips(
+  id serial PRIMARY KEY,
+  userID integer REFERENCES users NOT NULL,
+  title text NOT NULL,
+  content text,
+  created timestamp DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE tags(
+  id serial PRIMARY KEY,
+  userID integer REFERENCES users NOT NULL,
+  name text NOT NULL UNIQUE,
+  created timestamp DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE snips_tags(
+  snipID integer REFERENCES snips,
+  tagID integer REFERENCES tags,
+  created timestamp DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO users 
+  (username, password) VALUES
+    ('testUser', 'testPassword');
+
+INSERT INTO snips
+  (title, content, userID) VALUES
+    ('Testing Title', 'This be some content yo', 1);
+
+INSERT INTO tags
+  (name, userID) VALUES
+    ('javascript', 1);

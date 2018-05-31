6d6d84f41c4e6d4c18f4f8f4d394b883601b2820
Terrance Corley <terrancecorley@outlook.com>
2018-05-31 14:32:20 -0700

    created tag and snips api endpoints

--- routes/snips-router.js
+++ routes/snips-router.js
@@ -9,20 +9,63 @@ const router = express.Router();
 // Protect endpoints using JWT Strategy
 router.use('/', passport.authenticate('jwt', { session: false, failWithError: true }));
 
-router.get('/:userID', (req, res, next) => {
-  // get all snips from appropriate userID... and tags? (ask zach, this can probably be done separately without a problem)
-  
+router.get('/', (req, res, next) => {
+  const userID = req.user.id;
+
+  knex.select('snips.id', 'title', 'content', 'snips_tags.snipid as snipID', 'tags.id as tagID', 'tags.name as tagName')
+    .from('snips')
+    .where('snips.userid', userID)
+    .leftJoin('snips_tags','snips.id', 'snips_tags.snipid' )
+    .leftJoin('tags', 'tags.id', 'snips_tags.tagid')
+    .orderBy('snips.id')
+    .then(results => {
+      const result = results[0];
+      res.json(result);
+    })
+    .catch(err => next(err));
 });
 
-router.post('/:userID', (req, res, next) => {
-  // post snip for appropriate userID
+router.post('/', (req, res, next) => {
+  const { title, content, tags = [] } = req.body;
+  const userId = req.user.id;
+
+  /***** Never trust users - validate input *****/
+  if (!title) {
+    const err = new Error('Missing `title` in request body');
+    err.status = 400;
+    return next(err);
+  }
+
+  // if (tags) {
+  //   tags.forEach((tag) => {
+  //     if (Tag.find({_id: tag}).userId !== userId) {
+  //       const err = new Error('The item is not valid');
+  //       err.status = 401;
+  //       return next(err);
+  //     }
+  //   });
+  // }
+
+  knex
+    .insert({ title, content, tags, userid })
+    .into('snips')
+    .then(results => {
+      let result = results[0];
+      res
+        .location(`${req.originalUrl}/${result.id}`)
+        .status(201)
+        .json(result);
+    })
+    .catch(err => {
+      next(err);
+    });
 });
 
-router.put('/:userID', (req, res, next) => {
+router.put('/', (req, res, next) => {
   //update a snip
 });
 
-router.delete('/:userID', (req, res, next) => {
+router.delete('/', (req, res, next) => {
   // delete a snip
 });
 
--- routes/tags-router.js
+++ routes/tags-router.js
@@ -22,9 +22,16 @@ router.get('/', (req, res, next) => {
   .catch(err => next(err));
 });
 
+// post new tag to db
 router.post('/', (req, res, next) => {
   let userID = req.user.id;
   let name = req.body.tagName;
+
+   if (!name) {
+    const err = new Error('Missing `tagName` in request body');
+    err.status = 400;
+    return next(err);
+  }
 
   let newTag = {
     name,
@@ -39,14 +46,70 @@ router.post('/', (req, res, next) => {
       const result = results[0];
       res.location(`${req.originalUrl}/${result.id}`).status(201).json(result);
     })
+    .catch(err => {
+      if (err.code === 23505) {
+        err = new Error('Tag name already exists');
+        err.status = 400;
+      }
+      next(err);
+    });
 });
 
-router.put('', (req, res, next) => {
-  //update a tag name
-})
+// update tag name on db
+router.put('/:tagID', (req, res, next) => {
+  let id = req.params.tagID;
+  let userID = req.user.id;
+  let name = req.body.tagName;
 
-router.delete('', (req, res, next) => {
-  //delete a tag
+  if (!name) {
+    const err = new Error('Missing `tagName` in request body');
+    err.status = 400;
+    return next(err);
+  }
+
+  let updateTag = {
+    name
+  }
+
+  knex
+    .update(updateTag)
+    .from('tags')
+    .where({
+      id,
+      "userid": userID 
+    })
+    .returning(['id', 'name'])
+    .then( ([results]) => {
+      console.log(results);
+      res.json(results);
+    })
+    .catch(err => {
+      if (err.code === 23505) {
+        err = new Error('Tag name already exists');
+        err.status = 400;
+      }
+      next(err);
+    });
+});
+
+// delete tag from db
+router.delete('/:tagID', (req, res, next) => {
+  const id = req.params.tagID;
+  const userID = req.user.id;
+
+  knex
+    .from('tags')
+    .where({
+      id,
+      "userid": userID
+    })
+    .del()
+    .then( () => {
+      res.sendStatus(204);
+    })
+    .catch( (err) => {
+      next(err);
+    });
 });
 
 module.exports = router;

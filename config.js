ebe6b9d0920955c42c2ebf8b87cd1574a3f53cad
Terrance Corley <terrancecorley@outlook.com>
2018-07-06 12:13:22 -0700

    changed port 5000 to 3000

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   PORT: process.env.PORT || 8080,
-  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:5000',
+  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
   DATABASE_URL:
       process.env.DATABASE_URL || 'postgres://dev@localhost/dev-snips',
   TEST_DATABASE_URL:
